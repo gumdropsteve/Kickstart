7d1b58df977a934b89de27cd1532cf79845e2770
gumdropsteve <warobson@gmail.com>
2021-05-21 16:50:46 -0700

    add 'View Campaign' routing & page

--- routes.js
+++ routes.js
@@ -1,4 +1,8 @@
 const routes = require('next-routes')(); 
 // require() returns a function, 2nd () means function will be invoked immediately
 
+routes
+    .add('/campaigns/new', '/campaigns/new') // need this above so next line doesn't break `new` page
+    .add('/campaigns/:address', '/campaigns/show'); // address is wildcard, : let's know after is wildcard
+
 module.exports = routes; // export helpers for auto user navigation
