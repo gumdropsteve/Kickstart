fa3490b24c97d6cac8bad184a9f7a265fa424b6a
gumdropsteve <warobson@gmail.com>
2021-05-22 01:43:56 -0700

    build out routes

--- routes.js
+++ routes.js
@@ -3,6 +3,8 @@ const routes = require('next-routes')();
 
 routes
     .add('/campaigns/new', '/campaigns/new') // need this above so next line doesn't break `new` page
-    .add('/campaigns/:address', '/campaigns/show'); // address is wildcard, : let's know after is wildcard
+    .add('/campaigns/:address', '/campaigns/show') // address is wildcard, : let's know after is wildcard
+    .add('/campaigns/:address/requests', '/campaigns/requests/index')
+    .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
 
 module.exports = routes; // export helpers for auto user navigation
